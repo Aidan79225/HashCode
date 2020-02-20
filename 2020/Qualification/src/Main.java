fdcc82f56e7c6e6ad2829717f49cd522141f45aa
aidan.wang <aidan.wang@grindr.com>
2020-02-21 03:54:05 +0800

    Update Main.java

--- 2020/Qualification/src/Main.java
+++ 2020/Qualification/src/Main.java
@@ -57,12 +57,12 @@ public class Main {
         }
         //get max score
         Pair<Integer, UsedBooks> maxScoreIndex = getMaxScoreIndex(d, books, sections);
-        if (maxScoreIndex.first < 0) {
+        if (maxScoreIndex.first < 0 || maxScoreIndex.second.bookIds.size() == 0) {
             return;
         }
         ans.add(maxScoreIndex);
         Section section = sections.get(maxScoreIndex.first);
-        sections.remove(maxScoreIndex);
+        sections.remove(maxScoreIndex.first.intValue());
         //update sections
         removeBooks(maxScoreIndex.second, sections);
         //next round
@@ -70,12 +70,13 @@ public class Main {
     }
 
     private Pair<Integer, UsedBooks> getMaxScoreIndex(int d, List<Integer> books, List<Section> sections) {
-        int max = 0;
+        int max = Integer.MIN_VALUE;
         int cur = -1;
         UsedBooks curUsedBooks = new UsedBooks();
         for (int i = 0; i < sections.size(); i++) {
             UsedBooks usedBooks = getScore(d, books, sections.get(i));
             if (usedBooks.score > max) {
+                max = usedBooks.score;
                 cur = i;
                 curUsedBooks = usedBooks;
             }
